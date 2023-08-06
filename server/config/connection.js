61fb4b506a572735ffb09994ddda30a4946f22e2
bllongbeverly <bllongbeverly@gmail.com>
2023-08-06 16:25:07 -0500

    still trying to deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://bambitheb:$BriDan0293?@cluster0.e1eyore.mongodb.net');
+mongoose.connect("mongodb+srv://bambitheb:$BriDan0293?@cluster0.e1eyore.mongodb.net");
 
 module.exports = mongoose.connection;
